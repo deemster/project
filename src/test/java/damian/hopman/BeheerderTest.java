cb810b4af8822313fa89f43e1f6e90c072f42ca3
deemster <damianhopman@hotmail.com>
2021-06-01 14:06:29 +0200

    versie2.1

---
+++ src/test/java/damian/hopman/BeheerderTest.java
@@ -0,0 +1,12 @@
+package damian.hopman;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeheerderTest {
+    @Test
+    public void verwijder(){
+        assertEquals(2, 1+1);
+    }
+}
